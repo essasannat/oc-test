32af81ec30ee8842b6454912e7bae9513f124222
Essa Sannat <easa98@gmail.com>
2024-08-30 16:33:18 +0300

    add react-icons package

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { FaBeer } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-6xl"> Hello Openshift</div>
+      <div className="text-6xl">
+        {" "}
+        Hello Openshift <FaBeer />
+      </div>
       <p className="text-slate-100"> Added Pipeline action for CD </p>
     </main>
   );
