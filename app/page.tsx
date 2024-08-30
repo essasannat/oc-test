11f2f3d916f065b2232cb1cb9a431307f35b22a8
Essa Sannat <easa98@gmail.com>
2024-08-30 16:05:48 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-6xl"> Hello Openshift</div>
+      <p className="text-slate-100"> Just updated my code</p>
     </main>
   );
 }
