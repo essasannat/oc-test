e9a7bff2690ebfc6e1784389c295b7c8c8b91aa2
Essa Sannat <easa98@gmail.com>
2024-08-30 16:23:37 +0300

    update 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-6xl"> Hello Openshift</div>
-      <p className="text-slate-100"> Just updated my code</p>
+      <p className="text-slate-100"> Added Pipeline action for CD </p>
     </main>
   );
 }
